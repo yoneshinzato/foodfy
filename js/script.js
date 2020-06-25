1ae2bae8f314b85bf483628ff0e3471ee3f27c81
yoneshinzato <chibariyo@gmail.com>
2020-06-24 23:27:26 -0300

    finaliza modal com infos

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-const modalOverlay = document.querySelector('.modal-overlay')
+const modalOverlay = document.querySelector(".modal-overlay");
 
-const modal = document.querySelector('.modal')
+const modal = document.querySelector(".modal");
 
-const recipes = document.querySelector('.recipe')
+const recipes = document.querySelectorAll(".recipe");
 
 for (let recipe of recipes) {
-  recipe.addEventListener('click', function () {
-    modalOverlay.classList.add('active')
-  })
+  recipe.addEventListener("click", function () {
+    const recipeImgId = recipe.querySelector(".recipe-img").getAttribute("id");
+    modalOverlay.classList.add("active");
+    modal.querySelector("img").src = `/assets/${recipeImgId}.png`;
+
+    modal.querySelector(".modal-content h3").innerText = recipe.querySelector(
+      ".recipe-content h3"
+    ).innerText;
+
+    modal.querySelector(".modal-content p").innerText = recipe.querySelector(
+      ".recipe-content p"
+    ).innerText;
+  });
 }
+
+document.querySelector(".close-modal").addEventListener("click", function () {
+  modalOverlay.classList.remove("active");
+});
